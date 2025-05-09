0264ade9fdc70093d97ec3c33c3224bf7cb79317
Rafael Luis <132370430+RafaelBarrosaluno@users.noreply.github.com>
2025-05-08 21:17:36 -0300

    Add files via upload

---
+++ Form02/RespForm2.php
@@ -0,0 +1,13 @@
+
+
+<?php
+$vnome=$_POST["nome"];
+$vsobrenome=$_POST["sobrenome"];
+$vcidade=$_POST["cidade"];
+$vemail=$_POST["email"];
+echo " O nome é: <strong>$vnome</strong><br/>";
+echo " O Sobrenome é: <strong>$vsobrenome</strong><br/>";
+echo " A Cidade é: <strong>$vcidade</strong><br/>";
+echo " O E-mail é: <strong>$vemail</strong><br/><br/><br/>";
+echo"<hr/>";
+?>
