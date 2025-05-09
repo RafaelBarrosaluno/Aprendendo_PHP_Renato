2a7f93e7461eac60729a18bc57156a567258d4e9
Rafael Luis <132370430+RafaelBarrosaluno@users.noreply.github.com>
2025-05-08 21:16:53 -0300

    Add files via upload

---
+++ Form/RespForm.php
@@ -0,0 +1,10 @@
+<?
+<html>
+<head>
+<title>Recebendo e exibindo os dados</title>
+</head>
+<body>
+<p>Seja bem vindo <?php echo $_POST["nome"]; ?></p>
+<p>Você tem <?php echo $_POST["idade"]; ?> anos de idade!</p>
+</body>
+?>
