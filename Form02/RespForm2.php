bb1492d39f4848f295aaa95ef5d2f6b2ff9a59ff
Rafael Luis <132370430+RafaelBarrosaluno@users.noreply.github.com>
2025-05-08 21:19:00 -0300

    Update RespForm2.php

--- Form02/RespForm2.php
+++ Form02/RespForm2.php
@@ -10,4 +10,8 @@ echo " O Sobrenome é: <strong>$vsobrenome</strong><br/>";
 echo " A Cidade é: <strong>$vcidade</strong><br/>";
 echo " O E-mail é: <strong>$vemail</strong><br/><br/><br/>";
 echo"<hr/>";
+echo "<b>O nome é:</b> $vnome $vsobrenome<br/>";
+echo "<b>Cidade:</b> $vcidade <b>E-Mail:</b> $vemail";
+?>
+
 ?>
